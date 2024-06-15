b0a7ff262533954abb5aca849770993709e5a24e
otakugodie <95259091+otakugodie@users.noreply.github.com>
2024-06-15 21:12:01 +0100

    data from DB Query

--- server.js
+++ server.js
@@ -10,19 +10,26 @@ apiInstance.setApiKey(
 //console.log(process.env.API_KEY_MAILER);
 
 async function main() {
+  
+  //DBQuery
+  const user = {
+    name: 'Maria',
+    email: 'otakugodie@gmail.com'
+  };
+
   try {
     const sendSmtpEmail = new brevo.SendSmtpEmail();
 
     sendSmtpEmail.subject = "Helo world from brevo and NodeJS";
     sendSmtpEmail.to = [
       {
-        email: "diegofer.rodriguezc@gmail.com",
-        name: "DiegoR",
+        email: user.email,
+        name: user.name,
         email: "otakugodie@gmail.com",
         name: "Diego Rodriguez",
       },
     ];
-    sendSmtpEmail.htmlContent = "<html><body><h1>Hello world from Brevo and NodeJS</h1><p>This is a teste email</p><button>Click me</button><a href='https://www.google.com'>Visit Google</a></body></html>";
+    sendSmtpEmail.htmlContent = `<html><body><h1>Hello ${user.name} from Brevo and NodeJS</h1><p>This is a teste email</p><button>Click me</button><a href='https://www.google.com'>Visit Google</a></body></html>`;
         
         // ```
         //     <html>
